226bb195992b8806ca1dc2cb84968721dab8b2cd
Brady Guo <a800608a@gmail.com>
2024-05-08 17:46:05 +0800

    Add lesson8_cmake
    Wrote part of turtle_spawner

---
+++ ros2_ws/src/lesson8_cmake/include/lesson8_cmake/turtle_spawner.hpp
@@ -0,0 +1,54 @@
+/*******************************************************************************
+* Copyright 2024 BROGENT TECHNOLOGIES INC.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* Author    : Brady Guo
+* Maintainer: Brady Guo
+* Reference : https://google.github.io/styleguide/cppguide.html#Class_Format
+*******************************************************************************/
+#ifndef TURTLE_SPAWNER__HPP_
+#define TURTLE_SPAWNER__HPP_
+
+
+#include <rclcpp/rclcpp.hpp>
+#include <cmath>
+#include <random>
+#include <turtlesim/srv/spawn.hpp>
+#include <turtlesim/srv/kill.hpp>
+#include "lesson_interfaces/msg/turtle.hpp"
+#include "lesson_interfaces/msg/turtle_array.hpp"
+#include "lesson_interfaces/srv/catch_turtle.hpp"
+
+
+class TurtleSpawner: public rclcpp::Node{
+ public:
+  TurtleSpawner(
+    std::string node_name="turtle_spawner_node")
+      : Node(node_name) {
+    this->alive_turtles_publisher_ = this->create_publisher<lesson_interfaces::msg::TurtleArray>("alive_turtles",10);
+    this->spawn_turtle_timer_ = this->create_wall_timer(std::chrono::seconds(2),
+                                                        std::bind(&TurtleSpawner::callback_spawn_turtle_timer_,this));
+    this->turtle_spawn_client_ = this->create_client<turtlesim::srv::Spawn>("spawn");
+  }
+
+ private:
+    rclcpp::Publisher<lesson_interfaces::msg::TurtleArray>::SharedPtr alive_turtles_publisher_;
+    rclcpp::TimerBase::SharedPtr spawn_turtle_timer_;
+    rclcpp::Client<turtlesim::srv::Spawn>::SharedPtr turtle_spawn_client_;
+
+    void callback_spawn_turtle_timer_();
+
+};
+
+
+#endif
