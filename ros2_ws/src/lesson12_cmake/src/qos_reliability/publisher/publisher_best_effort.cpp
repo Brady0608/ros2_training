2f1c6ce1917c814fbf1bf13829543e1bc97a8274
Brady Guo <a800608a@gmail.com>
2024-05-27 17:21:14 +0800

    ADD: lesson12_cmake
    Added qos_reliability

---
+++ ros2_ws/src/lesson12_cmake/src/qos_reliability/publisher/publisher_best_effort.cpp
@@ -0,0 +1,30 @@
+/*******************************************************************************
+* Copyright 2024 BROGENT TECHNOLOGIES INC.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* Author    : Brady Guo
+* Maintainer: Brady Guo
+*******************************************************************************/
+
+#include "lesson12_cmake/qos_reliability/publisher.hpp"
+
+Publisher::Publisher(std::string node_name)
+    : Node(node_name) {
+        
+        this->msg_id_ = 0;
+        auto qos_profile = rclcpp::QoS(10).best_effort();
+        this->publisher_ = this->create_publisher<std_msgs::msg::String>("/qos_test", qos_profile);
+        this->timer_ = this->create_wall_timer(std::chrono::seconds(1), std::bind(&Publisher::callback_timer_, this));        
+
+
+}
