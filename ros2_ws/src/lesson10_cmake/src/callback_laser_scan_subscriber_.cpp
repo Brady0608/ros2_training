f805313f1fef51f0fac83f5631bdf7e9f543614d
Brady Guo <brady_guo@brogent.com>
2024-07-03 14:12:30 +0800

    Refactor: lesson10_cmake

--- ros2_ws/src/lesson10_cmake/src/callback_laser_scan_subscriber_.cpp
+++ ros2_ws/src/lesson10_cmake/src/callback_laser_scan_subscriber_.cpp
@@ -21,9 +21,9 @@
 void RetrieveLaserScan::callback_laser_scan_subscriber_(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
 
     double angle_range = this->get_parameter(this->scan_angle_range_parameter_).as_double();
-    double radian_range = (angle_range >= 270.0) ? 
-                           msg->ranges.size() * msg->angle_increment :
-                           angle_range*M_PI/180.0;
+    double radian_range = (angle_range >= 270.0) 
+                        ? msg->ranges.size() * msg->angle_increment 
+                        : angle_range*M_PI/180.0;
     int max_index = std::floor(radian_range / msg->angle_increment);
     auto publish_msg = std::make_shared<sensor_msgs::msg::LaserScan>(*msg);
     publish_msg->ranges.resize(max_index);
